9e491f479d719378121ea311f8c80f06a2d93ac2
madhu <madhu.d@translab.io>
2024-10-07 08:14:09 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/translab7-AZ/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "GauravResourceGroup"
+  resource_group_name = "myresourcegroup"
   # insert required variables here
 }
